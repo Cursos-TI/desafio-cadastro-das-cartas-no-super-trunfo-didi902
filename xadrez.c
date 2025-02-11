1eeffa4ce3a560bdaa1b863affb3d2cc97f86d91
didi902 <gismarjr92@gmail.com>
2025-02-11 22:03:40 +0000

    id

---
+++ xadrez.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 32;
+    float altura = 1.82;
+    int peso = 80;
+    char opcao = 'S';
+    char nome[32] = "gismar";
+
+    printf("idade: %d\n", idade);
+    printf("altura: %f\n", altura);
+    printf("peso: %d\n", peso);
+    printf("opcao: %c\n", opcao);
+    printf("nome: %s\n", nome);
+
+    return 0;
+}
