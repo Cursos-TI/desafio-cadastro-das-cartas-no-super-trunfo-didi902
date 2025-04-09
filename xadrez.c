3fa264cd2d7acdc17dce89b2a9cec8c328472858
didi902 <gismarjr92@gmail.com>
2025-04-09 01:09:20 +0000

    ainda nao

--- xadrez.c
+++ xadrez.c
@@ -2,81 +2,82 @@
 
 int main(){
 
-    char estado;
-    char codigo[50];
-    char cidade[50];
-    int populacao;
-    float area_km;
-    float PIB;
-    int pontos;
-    float densidade_populacional;
-    float PIB_per_capta;
-    int divisao = populacao / area_km;
-    int divisao = PIB_per_capta / populacao;
+    char estado01,estado02;
+    char codigo01[5],codigo02[5];
+    char cidade01[60],cidade02[60];
+    int populacao01,populacao02;
+    float area_km01,area_km02;
+    float PIB01,PIB02;
+    int pontos01,pontos02;
+    float densidade01,densidade02;
+    float PIB_per_capta01,PIB_per_capta02;
+    int pci = populacao01,populacao02 , areakm01,areakm02;
+    int pci = PIB_per_capta01,PIB_per_capta02 , populacao01,populacao02;
 
 
     printf("digite o seu estado: \n");
-    scanf("%s", &estado);
+    scanf("%s", &estado01);
     
     printf("digite o codigo da carta: \n");
-    scanf("%s", &codigo);
+    scanf("%s", &codigo01);
 
     printf("digite o nome da cidade: \n");
-    scanf("%s", &cidade);
+    scanf("%s", &cidade01);
 
     printf("digite o tamanho da populacao: \n");
-    scanf("%d", &populacao);
+    scanf("%d", &populacao01);
 
     printf("digite a area em km: \n");
-    scanf("%f", &area_km);
+    scanf("%f", &area_km01);
 
     printf("digite o PIB: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &PIB01);
 
     printf("digite a quantidade de pontos turisticos: \n");
-    scanf("%d", &pontos);
+    scanf("%d", &pontos01);
 
     printf("digite a densidade populacional: \n");
-    scanf("%f", &densidade_populacional);
+    scanf("%f", &densidade01);
 
     printf("digite o PIB per capta: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &PIB01);
 
-    printf("digite o numero da populacao e da area: \n");
-    scanf("divisao: %d\n", divisao);
+    //printf("digite o numero da populacao e da area: \n");
+    pci = (float)(populacao01 / area_km01);
 
-    printf("digite o PIB per capta e a populacao: \n");
-    scanf("divisao: %d\n", divisao);
+    //printf("digite o PIB per capta e a populacao: \n");
+    pci = (float)(PIB01 /populacao01);
 
+ 
 
     printf("digite as informacoes da seunda carta: \n");
 
     printf("digite o seu estado: \n");
-    scanf("%s", &estado);
+    scanf("%s", &estado02);
     
     printf("digite o codigo da carta: \n");
-    scanf("%s", &codigo);
+    scanf("%s", &codigo02);
 
     printf("digite o nome da cidade: \n");
-    scanf("%s", &cidade);
+    scanf("%s", &cidade02);
 
     printf("digite o tamanho da populacao: \n");
-    scanf("%d", &populacao);
+    scanf("%d", &populacao02);
 
     printf("digite a area em km: \n");
-    scanf("%f", &area_km);
+    scanf("%f", &area_km02);
 
     printf("digite o PIB: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &PIB02);
 
     printf("digite a quantidade de pontos turisticos: \n");
-    scanf("%d", &pontos);
+    scanf("%d", &pontos02);
 
     printf("digite a densidade populacional: \n");
-    scanf("%f", &densidade_populacional);
+    scanf("%f", &densidade02);
 
     printf("digite o PIB per capta: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &PIB02);
 
     return 0;
 
