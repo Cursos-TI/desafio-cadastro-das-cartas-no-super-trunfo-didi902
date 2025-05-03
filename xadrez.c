8bd8cc64056af419c8dc61a87a51f36ed2ead070
didi902 <gismarjr92@gmail.com>
2025-05-03 21:54:14 +0000

    final

--- xadrez.c
+++ xadrez.c
@@ -11,6 +11,7 @@ int main(){
     int pontos01, pontos02;
     float densidade01, densidade02;
     float PIB_per_capta01, PIB_per_capta02;
+    float somapoder01, somapoder02;
     
     printf("\carta01\n");
 
@@ -38,6 +39,8 @@ int main(){
     //calculo da densidade pelo pib
     densidade01 = populacao01 / areakm01;
     PIB01 = PIB01*1000000000 / (float)populacao01;
+
+    somapoder01 = ((float)populacao01 + areakm01 + PIB01 + pontos01 + PIB_per_capta01) + (1 / densidade01);
 
 
     
@@ -67,6 +70,8 @@ int main(){
     //calculo da densidade pelo pib
     densidade02 = populacao02 / areakm02;
     PIB02 = PIB02*1000000000 / (float)populacao02;
+
+    somapoder02 = ((float)populacao02 + areakm02 + PIB02 + pontos02 + PIB_per_capta02) + (1 / densidade02);
     
 
 
@@ -111,6 +116,37 @@ int main(){
     printf("densidade populacional: %.2f\n", densidade02);
 
     printf("PIB per capta:%.2f\n", PIB02);
+
+
+     printf("\nComparação de Cartas:\n");
+
+     printf("População: Carta %d venceu (%d)\n",
+            (populacao01 > populacao02) * 1 + (populacao02 >= populacao01) * 2,
+            populacao01 > populacao02);
+ 
+     printf("Área: Carta %d venceu (%d)\n",
+            (areakm01 > areakm02) * 1 + (areakm02 >= areakm01) * 2,
+            areakm01 > areakm02);
+ 
+     printf("PIB: Carta %d venceu (%d)\n",
+            (PIB01 > PIB02) * 1 + (PIB02 >= PIB01) * 2,
+            PIB01 > PIB02);
+ 
+     printf("Pontos Turísticos: Carta %d venceu (%d)\n",
+            (pontos01 > pontos02) * 1 + (pontos02 >= pontos01) * 2,
+            pontos01 > pontos02);
+ 
+     printf("Densidade Populacional: Carta %d venceu (%d)\n",
+            (densidade01 < densidade02) * 1 + (densidade01 > densidade02) * 2,
+            densidade01 < densidade02);
+ 
+     printf("PIB per Capita: Carta %d venceu (%d)\n",
+            (PIB_per_capta01 > PIB_per_capta02) * 1 + (PIB_per_capta02 >= PIB_per_capta01) * 2,
+            PIB_per_capta01 > PIB_per_capta02);
+ 
+     printf("Super Poder: Carta %d venceu (%d)\n",
+            (somapoder01 > somapoder02) * 1 + (somapoder02 >= somapoder01) * 2,
+            somapoder01 > somapoder02);
 
 
 
